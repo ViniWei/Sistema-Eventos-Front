c06357812965104630ced8c125907e041922a4c9
Vinícius Weigert <viniciusweigert152@gmail.com>
2023-11-20 11:47:38 -0300

    Backend Login Organizador

--- src/app/Pages/home/login-organizador/login-organizador.component.ts
+++ src/app/Pages/home/login-organizador/login-organizador.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-organizador',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-organizador.component.scss']
 })
 export class LoginOrganizadorComponent {
+  constructor(private router: Router, private http: HttpClient){}
+  apiUrl = 'http://localhost:5000/Organizador';
 
+  async login (email: string, senha: string){
+    const body = {
+      Email: email,
+      Senha: senha
+    }
+
+    this.http.post<any>(
+      `${this.apiUrl}/Login`,
+      body,
+      { responseType: 'json' }).subscribe(res => 
+        {
+          if (res.message == "Logado"){
+            this.router.navigateByUrl('homeOrganizador');
+          }
+        });
+  }
 }
