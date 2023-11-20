249f27dc9aa4f7fc352523a24c6224a09ded1df1
Altieris Souza <altmsf15@gmail.com>
2023-11-20 00:16:51 -0300

    Novas funcionalidades aos Kits

--- src/app/components/Produto/produtos/produtos.component.ts
+++ src/app/components/Produto/produtos/produtos.component.ts
@@ -17,7 +17,6 @@ export class ProdutosComponent {
   ngOnInit(): void {
     this.tituloFormulario = 'Produto';
     this.formulario = new FormGroup({  
-      id: new FormControl(0),
       nome: new FormControl('', Validators.required),
       descricao: new FormControl('', Validators.required),
       preco: new FormControl(0, [Validators.required, Validators.min(0)]),
@@ -31,7 +30,12 @@ export class ProdutosComponent {
   enviarFormulario(): void {
     const produto: Produto = this.formulario.value;
     this.produtoService.cadastrar(produto).subscribe(result => {
+      this.formulario.reset();
       alert('Produto inserido com sucesso.');
+    },
+    error => {
+      console.error('Erro ao inserir o produto:', error);
+      alert('Erro ao inserir o produto. Verifique o console para mais detalhes.');
     }
     )
   }
--- src/app/components/kit/kits/kits.component.ts
+++ src/app/components/kit/kits/kits.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Ingresso } from 'src/app/services/Ingresso/Ingresso';
+import { Kit } from 'src/app/services/Kit/Kit';
+import { KitsService } from 'src/app/services/Kit/kits.service';
+import { Produto } from 'src/app/services/Produto/Produto';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-kits',
   templateUrl: './kits.component.html',
   styleUrls: ['./kits.component.scss']
 })
-export class KitsComponent {
+export class KitsComponent implements OnInit{
+  formulario: any;
+  tituloFormulario: string = 'kits';
+  constructor(private location: Location, private kitsService: KitsService) { }
+
+
+  ngOnInit(): void {
+    this.tituloFormulario = 'Criar Kit';
+    this.formulario = new FormGroup({  
+      nome: new FormControl(null, Validators.required),
+      descricao: new FormControl(null, Validators.required),
+      preco: new FormControl(null, Validators.required),
+      organizadorId: new FormControl(null, Validators.required),
+      organizador: new FormControl(null, Validators.required),
+      produtos: new FormControl(null, Validators.required),
+    }
+    )
+  }
+
+  enviarFormulario(): void {
+    const kit: Kit = this.formulario.value;
+    this.kitsService.cadastrar(kit).subscribe(result => {
+      alert('Kit criado com sucesso.');
+    }
+    )
+  }
+
 
 }
